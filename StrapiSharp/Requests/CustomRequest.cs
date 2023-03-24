a7e3219a436bf60a20772e18e03ed4483e20fa6a
Alex Loren <lorenalexm@gmail.com>
2023-03-24 15:03:03 -0700

    Created custom request for user formed requests

---
+++ StrapiSharp/Requests/CustomRequest.cs
@@ -0,0 +1,24 @@
+﻿using StrapiSharp.Enums;
+using StrapiSharp.Models;
+
+namespace StrapiSharp.Requests
+{
+	/// <summary>
+	/// Request type used for all custom/user-defined requests against the Strapi server.
+	/// <c>GET/POST/PUT/DELETE /{contentType}</c>
+	/// </summary>
+	public class CustomRequest : RequestBase
+	{
+		/// <summary>
+		/// Creates the request.
+		/// </summary>
+		/// <param name="method">Which <see cref="RequestMethod"/> should this request use?</param>
+		/// <param name="contentType">The Strapi type of this request.</param>
+		/// <param name="path">A custom path to send this request to.</param>
+		public CustomRequest(RequestMethod method, string contentType, string path = "")
+			: base(method, contentType, path, null)
+		{
+		}
+	}
+}
+
