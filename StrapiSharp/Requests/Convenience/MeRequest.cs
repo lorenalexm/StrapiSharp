3767dcc8b2dcedb790db00989c130321913954d5
Alex Loren <lorenalexm@gmail.com>
2023-03-27 08:23:12 -0700

    Added request for getting current logged in user

---
+++ StrapiSharp/Requests/Convenience/MeRequest.cs
@@ -0,0 +1,21 @@
+﻿using StrapiSharp.Enums;
+using StrapiSharp.Models;
+using StrapiSharp.Requests;
+
+namespace StrapiSharp.Requests.Convenience;
+
+/// <summary>
+/// Convenience request type used for retrieving the logged in user.
+/// <c>GET /users/me</c>
+/// </summary>
+public class MeRequest : RequestBase
+{
+	/// <summary>
+	/// Creates the <see cref="MeRequest"/>.
+	/// </summary>
+	public MeRequest()
+		: base(RequestMethod.Get, "users", "/me")
+	{
+	}
+}
+
---
+++ StrapiSharpTests/Requests/Convenience/MeRequestTests.cs
@@ -0,0 +1,17 @@
+﻿namespace StrapiSharpTests.Requests.Convenience;
+
+public class MeRequestTests
+{
+	/// <summary>
+	/// Tests creating a new <see cref="MeRequest"/> and that properties match.
+	/// </summary>
+	[Test]
+	public void MeRequest()
+	{
+		var request = new MeRequest();
+		request.Method.Should().Be(RequestMethod.Get);
+		request.ContentType.Should().Be("users");
+		request.Path.Should().Be("/me");
+	}
+}
+
